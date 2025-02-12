8bdefe233e7058926e9b9556ec4bf406464970bf
meryemlitim <litimmeryem2001@gmail.com>
2025-02-12 16:59:02 +0100

    test

--- app/config/db.php
+++ app/config/db.php
@@ -1,31 +1,33 @@
-<?php
+<?php 
 
-class Db
-{
-  private $host = "roommate.mysql.database.azure.com";
-  private $port = "3306";
-  private $user = "team";
-  private $password = "Pfmtj5xuXSq;u'#";
-  private $database = "room-mate";
-  private $ssl_ca = "/DigiCertGlobalRootCA.crt.pem";
-  public $connection;
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'roommate'); 
+define('DB_USER', 'root');    
+define('DB_PASSWORD', '');    
 
-  public function __construct()
-  {
-    $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->database;charset=utf8mb4";
 
-    $options = [
-      PDO::MYSQL_ATTR_SSL_CA      => $this->ssl_ca, // Enable SSL
-      PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Set to true for stronger security
-      PDO::ATTR_ERRMODE           => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::ATTR_EMULATE_PREPARES  => false,
-    ];
 
-    try {
-      $this->connection = new PDO($dsn, $this->user, $this->password, $options);
-    } catch (PDOException $e) {
-      die("Connection failed: " . $e->getMessage());
+
+class Db {
+
+    protected $connection;
+
+    public function __construct()
+    {
+        try {
+            $this->connection = new PDO(
+                "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+                DB_USER,
+                DB_PASSWORD
+            );
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
     }
-  }
+
+    protected function getConnection() {
+        return $this->connection;
+    }
+
 }
